3743584d9fd7d390948dc29e350684326426a2ac
LuqAli123 <luali7239@oslomet.no>
2024-04-15 20:12:34 +0200

    .

--- src/main/java/com/example/billettoblig2/BillettController.java
+++ src/main/java/com/example/billettoblig2/BillettController.java
@@ -1,4 +1,12 @@
 package com.example.billettoblig2;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class BillettController {
+    public final List<Kinobillett> kinobilett= new ArrayList<>();
+
 }
--- src/main/java/com/example/billettoblig2/Kinobillett.java
+++ src/main/java/com/example/billettoblig2/Kinobillett.java
@@ -1,4 +1,68 @@
 package com.example.billettoblig2;
 
 public class Kinobillett {
+    private String film;
+    private String antall;
+    private String fornavn;
+    private String etternavn;
+    private String telefonnr;
+    private String epost;
+
+    public Kinobillett(String film, String antall, String fornavn,String etternavn, String telefonnr, String epost ){
+        this.antall = antall;
+        this.film = film;
+        this.fornavn = fornavn;
+        this.etternavn =  etternavn;
+        this.epost = epost;
+        this.telefonnr = telefonnr;
+    }
+
+    public String getFilm() {
+        return film;
+    }
+
+    public void setFilm(String film) {
+        this.film = film;
+    }
+
+    public String getAntall() {
+        return antall;
+    }
+
+    public void setAntall(String antall) {
+        this.antall = antall;
+    }
+
+    public String getFornavn() {
+        return fornavn;
+    }
+
+    public void setFornavn(String fornavn) {
+        this.fornavn = fornavn;
+    }
+
+    public String getEtternavn() {
+        return etternavn;
+    }
+
+    public void setEtternavn(String etternavn) {
+        this.etternavn = etternavn;
+    }
+
+    public String getTelefonnr() {
+        return telefonnr;
+    }
+
+    public void setTelefonnr(String telefonnr) {
+        this.telefonnr = telefonnr;
+    }
+
+    public String getEpost() {
+        return epost;
+    }
+
+    public void setEpost(String epost) {
+        this.epost = epost;
+    }
 }
+
